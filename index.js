78f91dfcdd2e3cf8326dba65c9b277e95fff9390
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-04-13 13:56:29 -0400

    commit tree 55d8eb631af72e1594a5e7bfa0ef37779eb7db72

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if (Array.isArray(collection)){
+        collection.forEach(item => callback(item))
+      } else {
+        Object.values(collection).forEach(item => callback(item))
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      if (Array.isArray(collection)){
+        return collection.map(item => callback(item))
+      } else {
+        return Object.values(collection).map(item => callback(item))
+      }
     },
 
     reduce: function() {
